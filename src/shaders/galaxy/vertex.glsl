f8b4b4e2e0bcd85ea1ac6e087d69bb0cebb6ca12
Urooook <kotomba.seleznev@yandex.ru>
2022-01-26 23:49:31 +0300

    galaxy animation

---
+++ src/shaders/galaxy/vertex.glsl
@@ -0,0 +1,36 @@
+uniform float uSize;
+uniform float uTime;
+
+attribute float aScale;
+attribute vec3 aRandomness;
+
+varying vec3 vColor;
+
+void main () {
+    vec4 modelPosition = modelMatrix * vec4(position, 1.0);
+
+    //Spin
+    float angle = atan(modelPosition.x, modelPosition.z);
+    float distanseToCenter = length(modelPosition.xz);
+    float angleoffset = (1.0 / distanseToCenter) * uTime * 0.2;
+    angle += angleoffset;
+
+    modelPosition.x = cos(angle) * distanseToCenter;
+    modelPosition.z = sin(angle) * distanseToCenter;
+
+    //Randomness
+    modelPosition.x += aRandomness.x;
+    modelPosition.y += aRandomness.y;
+    modelPosition.z += aRandomness.z;
+
+    vec4 viewPosition = viewMatrix * modelPosition;
+    vec4 projectionPosition = projectionMatrix * viewPosition;
+
+    gl_Position = projectionPosition;
+
+    gl_PointSize = uSize * aScale;
+    gl_PointSize *= (1.0 / -viewPosition.z);
+
+    //Color
+    vColor = color;
+}
